9bcd1d086aa25a0a1e2c3e965426f5f5c6df5b15
Tehseenkhann <92551223+Tehseenkhann@users.noreply.github.com>
2022-08-30 06:59:11 +0500

    Post & Get Method

--- 2dimention-array-prac.php
+++ 2dimention-array-prac.php
@@ -13,6 +13,10 @@ $cars=array
 echo $cars[0][0].": Ordered: ".$cars[0][1].". Sold: ".$cars[0][2]."<br>";
 echo $cars[1][0].": Ordered: ".$cars[1][1].". Sold: ".$cars[1][2]."<br>";
 echo $cars[2][0].": Ordered: ".$cars[2][1].". Sold: ".$cars[2][2]."<br>";
-?>
+
+
+
+
 </body>
 </html>
+
